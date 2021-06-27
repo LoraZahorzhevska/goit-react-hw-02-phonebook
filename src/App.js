511f4164e4df444766aa0ab20279e6a1eefff150
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-06-27 19:01:19 +0300

    завершает первый шаг

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import { tSImportEqualsDeclaration } from "@babel/types";
+// import { name } from "commander";
+import React, { Component } from "react";
+import shortid from "shortid";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { name: "Rosie Simpson" },
+      { name: "Hermione Kline" },
+      { name: "Eden Clements" },
+    ],
+    name: "",
+  };
+
+  idName = shortid.generate();
+
+  handleChange = (event) => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.PreventDefault();
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name{""}
+            <input
+              type="text"
+              name="name"
+              id={this.idName}
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(({ name }) => {
+            return <li key={name}>{name}</li>;
+          })}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
